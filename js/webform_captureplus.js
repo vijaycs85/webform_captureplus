07a630d598b5647e6383a7a41d3359a972a3d537
Vijayachandran Mani <vijaycs85@gmail.com>
2015-08-07 16:52:34 +0100

    More update and support for multiple address fields

--- js/webform_captureplus.js
+++ js/webform_captureplus.js
@@ -5,10 +5,11 @@
     Drupal.behaviors.webform_captureplus = Drupal.behaviors.webform_captureplus || {};
     Drupal.behaviors.webform_captureplus.attach = function() {
         // create quasi static var to save perfomance
-        $.each(Drupal.settings, function(key, info) {
-            if(key.substring(0, 20) == 'webform_captureplus_') {
-                info.control = new pca.Address(info.fields, info);
-
+        $.each(Drupal.settings, function(settings_id, settings) {
+            if(settings_id.substring(0, 20) == 'webform_captureplus_') {
+                $.each(settings, function(key, info){
+                    info.control = new pca.Address(info.fields, info);
+                });
             }
         });
         return;
