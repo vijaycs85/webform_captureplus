63254d50150d3db6f2b8103f75a0c33e72e9cb2b
Vijayachandran Mani <vijaycs85@gmail.com>
2016-02-17 15:14:41 +0000

    More update for placeholder text

--- js/webform_captureplus.admin.js
+++ js/webform_captureplus.admin.js
@@ -5,23 +5,23 @@
     Drupal.behaviors.webform_captureplus_admin = Drupal.behaviors.webform_captureplus_admin || {};
     Drupal.behaviors.webform_captureplus_admin = function(context) {
         $(document).ready(function(){
-            var $mode = '';
             $('#webform-components .mode').each(function(index){
                 placeholderElement($(this));
+               $(this).change(function(){
+                   placeholderElement($(this));
+               });
             });
-            $('#webform-components .mode').change(function(){
-                placeholderElement($(this));
-            })
         });
 
 
         function placeholderElement($element) {
+
             var $mode = $element.find(":selected").val();
             if ($mode == 1 || $mode == 3) {
-                $('.placeholder', $element.parent()).show();
+                $('.placeholder', $element.parents('tr')).show();
             }
             else {
-                $('.placeholder', $element.parent()).hide();
+                $('.placeholder', $element.parents('tr')).hide();
             }
         }
     };
